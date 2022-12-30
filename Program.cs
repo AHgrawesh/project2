f655310b109c99f2eff6a45b8616c075e476844f
AHgrawesh <121512891+AHgrawesh@users.noreply.github.com>
2022-12-30 21:01:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace project2
+{
+    class project2
+    {
+        static void Main(string[] args)
+        {
+            int x, y, sum;
+            int n1, n2;
+            Console.Write("Please, enter the lowest number : ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.Write("Please, enter the highest number : ");
+            n2 = int.Parse(Console.ReadLine());
+            Console.Write("The Perfect numbers between {0} and {1} are  : ", n1, n2);
+            for (x = n1; x <= n2; x++)
+            {
+                y = 1;
+                sum = 0;
+                while (y < x)
+                {
+                    if (x % y == 0)
+                        sum = sum + y;
+                    y++;
+                }
+                if (sum == x)
+                    Console.Write("\n{0} ", x);
+            }
+        }
+    }
+}
